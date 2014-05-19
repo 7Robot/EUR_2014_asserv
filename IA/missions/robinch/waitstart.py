3171974c066d381f34ea6ae809f7cf27c7c2ce0e
Ken <ken.hasselmann@etu.enseeiht.fr>
2014-05-19 14:16:43 +0200

    Update waitstart.py

--- IA/missions/robinch/waitstart.py
+++ IA/missions/robinch/waitstart.py
@@ -11,6 +11,7 @@ class WaitForSignal(Mission):
     def go(self, msg, state):
         logging.warn("starting mission %s" % self.name)
         if (state == 0 and msg.board == 'asserv' and msg.name == 'start'):
+            self.create_timer(85.0, 'fin_du_match'): #fin du match après 85sec (pour etre large)
             if msg.color == 0:
                 state = 127
             elif msg.color == 1:
