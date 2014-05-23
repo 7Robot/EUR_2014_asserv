378557bd232fd36c489d2c7a8f12d66be1152f1b
Ken <ken@kenh.fr>
2014-05-23 21:57:36 +0200

    mission lances

--- IA/missions/robinch/lances.py
+++ IA/missions/robinch/lances.py
@@ -10,34 +10,43 @@ class Lances(Mission):
         self.balls = 0
 
     def go(self, msg):
-        if (msg.board == 'internal' and msg.name == 'beginlances'):
+        if (self.state = 'off' and msg.board == 'internal' and msg.name == 'beginlances'):
             self.state = 'on'
             self.create_send_internal('forward', {'target':0.62, 'axe':'x', 'mission':self.name})
 
-        elif (self.state == 'on'):
-            if (msg.board == 'internal' and msg.name == 'done'):
+        elif (self.state == 'on' and msg.board == 'internal' and msg.name == 'done'):
+                self.asserv.odoBroadcastOn()
                 self.asserv.speed(0.05, 0.1, 0.1)
                 self.asserv.launchBalls(5)
                 self.state = "speed_lances"
 
-        elif (self.state == "speed_lances"):
-            if (msg.board == 'asserv' and msg.name == 'doneLaunch'):
+        elif (self.state == "speed_lances" and msg.board == 'asserv' and msg.name == 'doneLaunch'):
                 self.asserv.stop()
                 self.asserv.stopLaunch()
+                self.asserv.odoBroadcastOff()
                 self.state = 'off'
-            elif (msg.board == 'internal' and msg.name == 'alert'):
+
+        elif (self.state = 'speed_lances' and msg.board == 'internal' and msg.name == 'alert'):
                 self.asserv.stop()
                 self.asserv.stopLaunch()
-            elif (msg.board == 'internal' and msg.name == 'freepath'): 
+                self.state = 'alert'
+
+        elif (self.state = 'alert' and msg.board == 'internal' and msg.name == 'freepath'): 
                 self.asserv.getBalls()
-            elif (msg.board == 'asserv' and msg.name == 'balls'):
+                self.state = 'speed_lances_2'
+
+        elif (self.state = 'speed_lances_2' and msg.board == 'asserv' and msg.name == 'balls'):
                 self.balls = msg.amount
-                self.asserv.getPos()
-            elif (msg.board == 'asserv' and msg.name == 'pos'):
+                self.state = 'speed_lances_2'
+
+        elif (self.state = 'speed_lances_2' and msg.board == 'asserv' and msg.name == 'pos'):
                 if abs(msg.x) > 0.90:
+                    self.asserv.odoBroadcastOff()
+                    self.asserv.stopLaunch()
                     self.state = 'off'
                 elif abs(msg.x) < 0.90:
                     self.asserv.speed(0.05, 0.1, 0.1)
                     self.asserv.launchBalls(msg.amount)
+                    self.state = 'speed_lances'
 
 
