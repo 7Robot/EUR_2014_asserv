51d787a46b2f3e04b130253d400260939919eb9f
Pepino <pepino.francois@gmail.com>
2014-05-29 08:54:29 +0200

    commentaries

--- asserv_fatman/globals.h
+++ asserv_fatman/globals.h
@@ -1,7 +1,7 @@
 #ifndef GLOBALS_H
 #define	GLOBALS_H
 
-volatile unsigned int actionBras; // bus de bit du bras
+volatile unsigned int actionBras; // bus d'actions du bras
 volatile char numBras=1;
 
 
