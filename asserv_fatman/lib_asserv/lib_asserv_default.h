341ac577bf463aca2538c6301995c1ee7dcd4a25
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2014-05-31 10:15:31 +0200

    modif des vitesses et accelerations max

--- asserv_fatman/lib_asserv/lib_asserv_default.h
+++ asserv_fatman/lib_asserv/lib_asserv_default.h
@@ -11,11 +11,11 @@
 /*############################################################################*/
 
 #ifndef DEFAULT_CONSTRAINT_V_MAX
-#define DEFAULT_CONSTRAINT_V_MAX {0.5, 3}
+#define DEFAULT_CONSTRAINT_V_MAX {0.5, 1.5}
 #endif
 
 #ifndef DEFAULT_CONSTRAINT_A_MAX
-#define DEFAULT_CONSTRAINT_A_MAX {0.6, 2, 0.15}
+#define DEFAULT_CONSTRAINT_A_MAX {0.6, 1.4, 0.10}
 #endif
 
 #ifndef BLOCK_LIMIT
